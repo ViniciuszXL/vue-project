ea88e059f2dfbea9e0949dd8dd04aa3ca25f968f
ViniciuszXL <pessoal.marcusvinicius@outlook.com.br>
2024-02-19 23:02:47 -0400

    Testes concluídos das rotas

---
+++ backend/test/data.js
@@ -0,0 +1,10 @@
+module.exports = {
+    APP_PORT: 4005,
+    URLS: {
+        AUTHENTICATE: "/admin/authenticate",
+        USERS_LIST: "/user/list",
+        USER_CREATE: "/user/create",
+        USER_UPDATE: "/user/update/",
+        USER_DELETE: "/user/delete/"
+    }
+}
